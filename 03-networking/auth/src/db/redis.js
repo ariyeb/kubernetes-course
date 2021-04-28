5abb0f8fc477d1e13f065b6f23467e1dc603b7c9
Ariye Bar-tov <ariyeb@gmail.com>
2021-04-28 20:05:32 +0300

    pod to pod networking

--- 03-networking/auth/src/db/redis.js
+++ 03-networking/auth/src/db/redis.js
@@ -4,8 +4,8 @@ const { promisifyAll } = require('bluebird');
 promisifyAll(redis);
 
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    host: process.env.REDIS_SERVICE_SERVICE_HOST,
+    port: process.env.REDIS_SERVICE_SERVICE_PORT
 });
 
 module.exports = redisClient;
